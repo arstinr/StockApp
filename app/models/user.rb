8247b857c74f34ee9671bd5a9a480edae1846ca6
Arstin Reano <arstingabrelreano@gmail.com>
2024-11-11 07:59:25 +0800

    Update User model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  has many :stocks
+  has many :transactions
 end
