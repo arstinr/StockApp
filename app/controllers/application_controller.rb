c285a20760f7a04ea4e888554271a125909419f9
Arstin Reano <arstingabrelreano@gmail.com>
2024-11-12 00:45:22 +0800

    Typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
-  def after_sign_in_path_for(resources)
+  def after_sign_in_path_for(resource)
     if resource.is_admin?
       admin_root_path
     else
