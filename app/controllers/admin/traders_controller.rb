1dfd567d5cedf06ed5683c96344de4736af3acaf
Arstin Reano <arstingabrelreano@gmail.com>
2024-11-11 19:20:41 +0800

    Notes and edits

--- app/controllers/admin/traders_controller.rb
+++ app/controllers/admin/traders_controller.rb
@@ -1,22 +1,41 @@
 class Admin::TradersController < ApplicationController
   def index
+    @traders = User.traders
   end
 
   def show
-  end
-
-  def edit
+    @trader = User.traders.find(params[:id])
   end
 
   def new
+    @trader = User.new
   end
 
   def create
+    @trader = User.new(trader_params)
+    @trader.skip_confirmation! #SKIP CONFIRMABLE????
+    if @trader.save
+      redirect_to admin_traders_path, notice: "Trader account succesfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @trader = User.traders.find(params[:id])
   end
 
   def update
+    @trader = User.traders.find(params[:id])
+    if @trader.update(trader_params)
+      redirect_to admin_traders_path, notice: "Trader account succesfully created!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
+
+  private
+    def trader_params
+      params.require(:user).permit(:email, :name, :password, :is_admin)
+    end
 end
