e0b035b221d79b3bf199f0e80287c72b2d2d4dc3
Arstin Reano <arstingabrelreano@gmail.com>
2024-11-11 08:07:55 +0800

    Model validations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,8 @@
 class Stock < ApplicationRecord
   belongs_to :user
+  has_many :transactions
+
+  # validates :symbol, presence: true, uniqueness: true
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :stock_price, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :stock
+
+  validates :stock_name, presence: true
+  validates :order_quantity, numericality: { greater_than: 0 }
+  validates :order_price, numericality: { greater_than_or_equal_to: 0 }
 end
