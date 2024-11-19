05bcb7f677eaf4402c1e9daa410e32fcdfdd93a6
Arstin Reano <arstingabrelreano@gmail.com>
2024-11-19 16:57:52 +0800

    Initial search action commit

--- app/controllers/trader/stocks_controller.rb
+++ app/controllers/trader/stocks_controller.rb
@@ -2,11 +2,19 @@ class Trader::StocksController < ApplicationController
   def index
     @stocks = current_user.stocks
   end
-  
+
   def show
     @stock = current_user.stocks.find(params[:id])
   end
 
   def search
+
+    if params[:symbol].present?
+      api = AlphaVantageApi.new
+      @stock_data = api.fetch_stock_data(params[:symbol])
+    else
+      flash[:alert] = "Failed to fetch stock data"
+    end
+
   end
 end
