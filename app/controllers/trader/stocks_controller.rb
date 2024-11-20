1dbfa35b1a3802cfdfa8d1c5b142112e2fade31c
Arstin Reano <arstingabrelreano@gmail.com>
2024-11-20 15:15:33 +0800

    Initial Handle buy action

--- app/controllers/trader/stocks_controller.rb
+++ app/controllers/trader/stocks_controller.rb
@@ -18,4 +18,49 @@ class Trader::StocksController < ApplicationController
     end
   end
 
+  def create
+    symbol = params[:symbol]
+    price = params[:price]
+    quantity = params[:quantity]
+    action_type = params[:action_type]
+
+    if action_type == 'buy'
+      handle_buy(stock_symbol, current_price, quantity)
+    elsif action_type == 'sell'
+      handle_sell(stock_symbol, current_price, quantity)
+    else
+      flash[:alert] = "Invalid action. Please try again."
+    end
+
+    redirect_to trader_stocks_path
+  end
+
+  private
+  def handle_buy(symbol, price, quantity)
+    total_price = price * quantity
+
+    if current_user.balance >= total_price
+      current_user.update(balance: current_user.balance - total_price)
+
+      stock = current_user.stock.find_or_initialize_by(symbol: stock_symbol)
+      stock.quantity += quantity
+      stock.save!
+
+      current_user.transactions.create!(
+        stock_name: symbol,
+        stock_price: price,
+        order_quantity: quantity,
+        order_price: total_price,
+        #ADD transaction_type: 'buy'
+      )
+      
+      flash[:notice] = "Successfully bought #{quantity} of #{stock_symbol}."
+    else
+      flash[:alert] = "Not enough balance :<<<"
+    end
+  end
+
+  def handle_sell(symbol, price, quantity)
+  end
+
 end
